3d4ee6c71b272fe8b185d7774fe3f95198127d1e
Dat Nguyen <dnguyenk2@gmail.com>
2022-05-28 20:31:56 -0700

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,9 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */
+        links.map(ele => <a href={'#' + ele} key={ele}>{ele}</a>)
+        }</nav>;
 }
 
 export default NavBar;
